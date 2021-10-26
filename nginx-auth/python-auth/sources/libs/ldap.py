22dd2f7ee8cc725b9e9f43adacde3a453733275e
Marcel Tricolici <marcel@bigdatafed.com>
2021-10-26 16:28:17 +0300

    change indentation from 2 to 4 spaces

--- nginx-auth/python-auth/sources/libs/ldap.py
+++ nginx-auth/python-auth/sources/libs/ldap.py
@@ -4,36 +4,35 @@ from .ldap_user import LdapUser
 
 class LdapHelper:
 
-  def __init__(self, host, port):
-    self.__ldap_host = host
-    self.__ldap_port = port
+    def __init__(self, host, port):
+        self.__ldap_host = host
+        self.__ldap_port = port
 
-  def connect(self, user, password):
-    try:
-      self.__ldap = ldap.initialize('ldap://{0}:{1}'.format(
-        self.__ldap_host, self.__ldap_port))
-      self.__ldap.simple_bind_s(user, password)
-      return True
-    except ldap.INVALID_CREDENTIALS:
-      print("Error: invalid credentials")
-      return False
+    def connect(self, user, password):
+        try:
+            self.__ldap = ldap.initialize('ldap://{0}:{1}'.format(
+                self.__ldap_host, self.__ldap_port))
+            self.__ldap.simple_bind_s(user, password)
+            return True
+        except ldap.INVALID_CREDENTIALS:
+            print("Error: invalid credentials")
+            return False
 
-  def fetch_users(self, baseDn):
-    searchResult = self.__ldap.search_s(baseDn, ldap.SCOPE_ONELEVEL,
-      '(objectClass=inetOrgPerson)')
-    users=[]
-    for obj in searchResult:
-      user=LdapUser(
-        dn=obj[0],
-        cn='ceva',
-        user_id='ceva',
-        group_id='ceva',
-        mail='ceva')
-      #user.dn = obj[0]
-      #user.cn=obj[1]['cn'][0]
-      #print(type(obj[1]['cn'][0]))
-      #exit(0)
-      #user.groupId=obj[1]['gidNumber'][0]
-      users.append(user)
-      #print(obj)
-    return users
+    def fetch_users(self, baseDn):
+        searchResult = self.__ldap.search_s(baseDn, ldap.SCOPE_ONELEVEL,
+                '(objectClass=inetOrgPerson)')
+        users=[]
+        for obj in searchResult:
+            users.append(LdapUser(
+                    dn=obj[0],
+                    cn='ceva',
+                    user_id='ceva',
+                    group_id='ceva',
+                    mail='ceva'))
+        return users
+#user.dn = obj[0]
+#user.cn=obj[1]['cn'][0]
+#print(type(obj[1]['cn'][0]))
+#exit(0)
+#user.groupId=obj[1]['gidNumber'][0]
+#print(obj)
--- nginx-auth/python-auth/sources/main.py
+++ nginx-auth/python-auth/sources/main.py
@@ -5,8 +5,8 @@ from libs.ldap import LdapHelper
 ldap=LdapHelper('192.168.100.24', 1389)
 
 if (ldap.connect('cn=admin,dc=zuzu,dc=com', '123')):
-  res = ldap.fetch_users('ou=users,dc=zuzu,dc=com')
-  print("\n\n")
-  print(res)
+    res = ldap.fetch_users('ou=users,dc=zuzu,dc=com')
+    print("\n\n")
+    print(res)
 
-print("end")
+    print("end")
