9427596a0b31975b3075b74aa50822e802830a33
Marcel Tricolici <marcel@bigdatafed.com>
2021-10-26 13:31:36 +0300

    ldap search works ;) without auth? why?????

--- nginx-auth/python-auth/sources/libs/ldap.py
+++ nginx-auth/python-auth/sources/libs/ldap.py
@@ -1,7 +1,8 @@
+import ldap
 
 class LdapHelper:
   def zuzu1(self):
+    l = ldap.initialize('ldap://192.168.100.24:1389')
+    res = l.search_s('ou=users,dc=zuzu,dc=com', ldap.SCOPE_ONELEVEL)
+    print(res)
     print("zuzu1")
-
-  def zuzu2():
-    print("zuzu2")
--- nginx-auth/python-auth/sources/main.py
+++ nginx-auth/python-auth/sources/main.py
@@ -4,8 +4,7 @@ from libs.ldap import LdapHelper
 
 print("preved")
 
-#p=LdapHelper() << create an instance of a class
-#p.zuzu1() << call simple method
-#LdapHelper.zuzu2() << call a static method
+lh=LdapHelper()
+lh.zuzu1()
 
 print("end")
