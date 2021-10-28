0f7305222a745014971f6631193f346949b790ca
Marcel Tricolici <marcel@bigdatafed.com>
2021-10-28 11:37:00 +0300

    implement ldap synchroniztion every X seconds.

--- nginx-auth/python-auth/sources/main.py
+++ nginx-auth/python-auth/sources/main.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
 import logging
+import signal
+
 from libs.logger import AppLogger
 from libs.config import AppConfig
 from libs.ldap.data import LdapData
+from libs.ldap.sync import *
+
+def shutdown(signum, frame):
+    logging.info("!!!signal handler '%s'", signum)
+    stop_synchronization_job()
+    exit(0)
+
+signal.signal(signal.SIGINT, shutdown) # handle ctrl+c
 
 AppLogger.initialize()
 AppConfig.load()
 
 LdapData.fetch_data()
 print(LdapData.users)
+
+start_ldap_synchronization_job()
+
+input("")
